97a449594842ea390aca39e6fafd70fcb63ac908
DarkDranz <patrick96@hotmail.fr>
2021-08-23 13:28:57 +0200

    Project Creation

---
+++ Source/AmenBreakers/AmenBreakersGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AmenBreakersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AMENBREAKERS_API AAmenBreakersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
